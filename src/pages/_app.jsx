9d3f15e65892b603c8791186a5f7a57ee01854de
Gayuh kautaman <gkoma444@gmail.com>
2023-02-19 02:31:32 +0700

    feat: fixed typing

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,5 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
+import "@/styles/globals.css";
 
 import "regenerator-runtime/runtime";
 
@@ -9,7 +8,7 @@ const supportedChainIds = [1, 4, 137];
 const connectors = {
   injected: {},
 };
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
   return (
     <ThirdwebWeb3Provider
       supportedChainIds={supportedChainIds}
