c79be1684255a894cc169e9ab8853426da1b80c5
Gayuh kautaman <gkoma444@gmail.com>
2023-02-19 03:16:15 +0700

    feat:fixed props

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,14 @@ import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
 import "@/styles/globals.css";
 
 import "regenerator-runtime/runtime";
+import { AppProps } from "next/app";
 
 const supportedChainIds = [1, 4, 137];
 
 const connectors = {
   injected: {},
 };
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebWeb3Provider
       supportedChainIds={supportedChainIds}
