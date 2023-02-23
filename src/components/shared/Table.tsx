574d7294d44cddae8c138ac26d1e6318cbc842ac
Gayuh kautaman <gkoma444@gmail.com>
2023-02-23 15:54:12 +0700

    feat: added transaction history

---
+++ src/components/shared/Table.tsx
@@ -0,0 +1,187 @@
+import Sort from "../svg/sort";
+
+interface TableProps {
+  readonly headers: any;
+  readonly values: any;
+  //   readonly title: string;
+  readonly isEdit: boolean;
+}
+
+const Table = ({ headers, values, isEdit }: TableProps) => {
+  return (
+    <>
+      <section className="mt-5 mx-auto">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <div>
+            <div className="flex items-center gap-x-3">
+              <span className="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full dark:bg-gray-800 dark:text-blue-400">
+                {} transactions
+              </span>
+            </div>
+
+            <p className="mt-1 text-sm text-gray-500 dark:text-gray-300">
+              Your transaction history that are recorded on the Ethereum
+              main-net
+            </p>
+          </div>
+        </div>
+
+        <div className="mt-6 md:flex md:items-center md:justify-between">
+          <div className="inline-flex overflow-hidden bg-white border divide-x rounded-lg dark:bg-gray-900 rtl:flex-row-reverse dark:border-gray-700 dark:divide-gray-700">
+            <button className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 bg-gray-100 sm:text-sm dark:bg-gray-800 dark:text-gray-300">
+              View all
+            </button>
+
+            <button className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
+              Monitored
+            </button>
+
+            <button className="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
+              Unmonitored
+            </button>
+          </div>
+
+          <div className="relative flex items-center mt-4 md:mt-0">
+            <span className="absolute">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-5 h-5 mx-3 text-gray-400 dark:text-gray-600"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                />
+              </svg>
+            </span>
+
+            <input
+              type="text"
+              placeholder="Search"
+              className="block w-full py-1.5 pr-5 text-gray-700 bg-white border border-gray-200 rounded-lg md:w-80 placeholder-gray-400/70 pl-11 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+        </div>
+        {/* table */}
+        <div className="flex flex-col mt-6">
+          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+              <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                  <thead className="bg-gray-50 dark:bg-gray-800">
+                    <tr>
+                      {headers.map((head, idx) => (
+                        <th
+                          key={idx}
+                          scope="col"
+                          className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                        >
+                          <button className="flex items-center gap-x-3 focus:outline-none">
+                            <span>{head}</span>
+                            {idx >= 1 ? <></> : <Sort />}
+                          </button>
+                        </th>
+                      ))}
+                      {isEdit ? (
+                        <th scope="col" className="relative py-3.5 px-4">
+                          <span className="sr-only text-dark">Edit</span>
+                        </th>
+                      ) : (
+                        <></>
+                      )}
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
+                    {values.map((value, idx) => (
+                      <>
+                        <tr key={idx}>
+                          {Object.values(value).map((item: any, id) => {
+                            if (id < headers.length) {
+                              return (
+                                <>
+                                  <td
+                                    key={id}
+                                    className="px-4 py-4 text-sm font-medium whitespace-nowrap"
+                                  >
+                                    <div>
+                                      <p className="font-medium text-gray-800 dark:text-white ">
+                                        {item}
+                                      </p>
+                                    </div>
+                                  </td>
+                                </>
+                              );
+                            }
+                          })}
+                        </tr>
+                      </>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-6 sm:flex sm:items-center sm:justify-between ">
+          <div className="text-sm text-gray-500 dark:text-gray-400">
+            Page{" "}
+            <span className="font-medium text-gray-700 dark:text-gray-100">
+              1 of 10
+            </span>
+          </div>
+
+          <div className="flex items-center mt-4 gap-x-4 sm:mt-0">
+            <a
+              href="#"
+              className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md sm:w-auto gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-5 h-5 rtl:-scale-x-100"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+                />
+              </svg>
+
+              <span>previous</span>
+            </a>
+
+            <a
+              href="#"
+              className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md sm:w-auto gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800"
+            >
+              <span>Next</span>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-5 h-5 rtl:-scale-x-100"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+export default Table;
--- src/pages/[slug]/transaction-history.tsx
+++ src/pages/[slug]/transaction-history.tsx
@@ -4,11 +4,16 @@ import { useWeb3 } from "@3rdweb/hooks";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Card from "@/components/shared/Card";
-const Transaction = ({ wallet_address }) => {
+import axios from "axios";
+import Table from "@/components/shared/Table";
+
+const Transaction = ({ wallet_address, transaction_history }) => {
   const { address } = useWeb3();
   const router = useRouter();
   const [isAuth, setAuth] = useState(false);
-
+  const header = Object.keys(transaction_history.result[0]).slice(0, 10);
+  const values = Object.values(transaction_history.result);
+  console.log(values);
   useEffect(() => {
     if (wallet_address != address) {
       router.push("/");
@@ -24,6 +29,9 @@ const Transaction = ({ wallet_address }) => {
           <section>
             <Card>
               <h1>Transaction History</h1>
+              <div>
+                <Table headers={header} values={values} isEdit={false} />
+              </div>
             </Card>
           </section>
         </main>
@@ -34,9 +42,13 @@ const Transaction = ({ wallet_address }) => {
 
 export const getServerSideProps = async ({ params }) => {
   const wallet_address = params.slug;
-  // check wallet address
+  // get transaction history
+  const transaction_history = await axios.get(
+    `https://api.etherscan.io/api?module=account&action=txlist&address=0xc5102fE9359FD9a28f877a67E36B0F050d81a3CC&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=${process.env.ETHERSCAN_KEY}`
+  );
+
   return {
-    props: { wallet_address },
+    props: { wallet_address, transaction_history: transaction_history.data },
   };
 };
 export default Transaction;
