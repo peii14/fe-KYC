f7670a59c49d73cf0a63200887e25255ad3f2984
Gayuh kautaman <gkoma444@gmail.com>
2023-03-28 13:12:49 +0700

    faeat: refractor

--- src/lib/kyc.ts
+++ src/lib/kyc.ts
@@ -10,7 +10,6 @@ async function getContract() {
   const connectionProfilePath = path.resolve(process.cwd(), 'src/lib/connection.json');
   const connectionProfile = JSON.parse(fs.readFileSync(connectionProfilePath, 'utf8'));
 
-  // TODO: work on identity
   const connectionOptions = {
     wallet,
     identity: 'BCA', // Replace this with the user identity you have enrolled and added to the wallet
@@ -22,21 +21,24 @@ async function getContract() {
   return network.getContract('eKYC');
 }
 
-export async function submitKycData(customerId, kycData) {
+async function executeContractTransaction(transactionName, ...args) {
   try {
     const contract = await getContract();
-    const result = await contract.submitTransaction('submitKycData', customerId, kycData);
+    const result = await contract.submitTransaction(transactionName, ...args);
     return result.toString();
   } catch (error) {
     throw error;
   }
 }
-export async function createUserProfile(userId,ethereumAddress){
-  try {
-    const contract = await getContract();
-    const result = await contract.submitTransaction('createUserProfile', userId,ethereumAddress);
-    return result.toString();
-  } catch (error) {
-    throw error;
-  }
+
+export async function submitKycData(customerId, kycData) {
+  return executeContractTransaction('submitKycData', customerId, kycData);
+}
+
+export async function createUserProfile(userId, ethereumAddress) {
+  return executeContractTransaction('createUserProfile', userId, ethereumAddress);
+}
+
+export async function getUserProfile(userId) {
+  return executeContractTransaction('getUserProfile', userId);
 }
